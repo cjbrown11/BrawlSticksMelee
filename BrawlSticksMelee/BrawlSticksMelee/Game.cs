dfd59e206bd90a7b976b4a6cfb33dff32dedf1aa
Christopher Brown <cjbrown1@ksu.edu>
2023-11-08 00:04:06 -0600

    Basic Character Movement

---
+++ BrawlSticksMelee/BrawlSticksMelee/Game.cs
@@ -0,0 +1,55 @@
+﻿using BrawlSticksMelee.Sprites;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BrawlSticksMelee
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private StickmanSprite stickman;
+
+        public Game()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            stickman = new StickmanSprite(new Vector2(0, graphics.PreferredBackBufferHeight - 100));
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            stickman.LoadContent(Content);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            stickman.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            stickman.Draw(gameTime, spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ BrawlSticksMelee/BrawlSticksMelee/Sprites/StickmanSprite.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+
+namespace BrawlSticksMelee.Sprites
+{
+    public class StickmanSprite
+    {
+        private KeyboardState keyboardState;
+
+        private MouseState currentMouseState;
+
+        private MouseState previousMouseState;
+
+        private Texture2D stanceOne;
+
+        private Texture2D stanceTwo;
+
+        private Texture2D punchOne;
+
+        private Texture2D punchTwo;
+
+        private Texture2D[] stanceFrame;
+
+        private Vector2 position;
+
+        private bool flipped;
+
+        private double animationTimer;
+
+        private short animationFrame = 0;
+
+        public StickmanSprite(Vector2 startingPosition)
+        {
+            position = startingPosition;
+        }
+
+        /// <summary>
+        /// Loads the sprite texture using the provided ContentManager
+        /// </summary>
+        /// <param name="content">The ContentManager to load with</param>
+        public void LoadContent(ContentManager content)
+        {
+            stanceFrame = new Texture2D[]
+            {
+                stanceOne = content.Load<Texture2D>("StickmanSprite-StanceOne"),
+                stanceTwo = content.Load<Texture2D>("StickmanSprite-StanceTwo"),
+                punchOne = content.Load<Texture2D>("StickmanSprite-PunchOne"),
+                punchTwo = content.Load<Texture2D>("StickmanSprite-PunchTwo")
+            };
+        }
+
+        /// <summary>
+        /// Updates the sprite's position based on user input
+        /// </summary>
+        /// <param name="gameTime">The GameTime</param>
+        public void Update(GameTime gameTime)
+        {
+            keyboardState = Keyboard.GetState();
+
+            // Apply keyboard movement
+            if ((keyboardState.IsKeyDown(Keys.Left) || keyboardState.IsKeyDown(Keys.A)) && position.X > 0)
+            {
+                position += new Vector2(-2, 0);
+                flipped = true;
+            }
+            if ((keyboardState.IsKeyDown(Keys.Right) || keyboardState.IsKeyDown(Keys.D)) && position.X < 670)
+            {
+                position += new Vector2(2, 0);
+                flipped = false;
+            }
+        }
+
+        /// <summary>
+        /// Draws the sprite using the supplied SpriteBatch
+        /// </summary>
+        /// <param name="gameTime">The game time</param>
+        /// <param name="spriteBatch">The spritebatch to render with</param>
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            previousMouseState = currentMouseState;
+
+            currentMouseState = Mouse.GetState();
+
+            // Update animation timer
+            animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            // Update animation frame
+            
+            
+            if (previousMouseState.LeftButton == ButtonState.Released && currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                animationFrame = 2;
+                animationTimer = 0;
+            }
+            else if (animationTimer > 0.4)
+            {
+                if (animationFrame == 2)
+                {
+                    animationFrame++;
+                    animationTimer = 0;
+                }
+                else if (animationFrame == 3)
+                {
+                    animationFrame = 0;
+                    animationTimer = 0;
+                }       
+                else
+                {
+                    animationFrame++;
+                    if (animationFrame > 1) animationFrame = 0;
+                    animationTimer = 0;
+                }
+                
+            }
+            
+
+            SpriteEffects spriteEffects = (flipped) ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+            spriteBatch.Draw(stanceFrame[animationFrame], position, null, Color.White, 0, new Vector2(0, 0), 2, spriteEffects, 0);
+        }
+    }
+}
